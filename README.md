f887119ebfa9be389d926da4046cc3cc5b05931e
sortira <aritroshomework@gmail.com>
2024-10-24 16:48:47 +0530

    Added all the files for alpha version

---
+++ README.md
@@ -0,0 +1,13 @@
+# Where Am I?
+
+## Description
+A GeoGuessr spinoff with minimalistic features and anti-dopamine-trigger features. My entry for the WebGame challenge at IIEST Shibpur, 2024.
+
+## Installation
+
+```bash
+git clone https://github.com/sortira/whereami.git
+chrome index.html
+```
+
+
---
+++ about.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+    // Create the floating box element
+    const floatingBox = document.createElement("div");
+    floatingBox.id = "floatingBox";
+
+    // Box content (text and buttons)
+    floatingBox.innerHTML = `
+        <div class="box-content">
+            <p class="lead">If you are the judge, which one are you?</p>
+            <button class="btn btn-custom" id="btnJudge1">UI/UX Lead</button>
+            <button class="btn btn-custom" id="btnJudge2">Dev Lead</button>
+            <button class="btn btn-custom" id="btnJudge3">None</button>
+        </div>
+    `;
+
+    // Append the floating box to the body
+    document.body.appendChild(floatingBox);
+
+    // Add event listeners to the buttons to hide the box on click
+    document.getElementById("btnJudge1").onclick = function() {
+        window.location.href = 'uiux.html';
+    };
+    document.getElementById("btnJudge2").onclick = function() {
+        window.location.href = 'devlead.html';    
+    };
+    document.getElementById("btnJudge3").onclick = function() {
+        floatingBox.style.display = "none";
+    };
+};
